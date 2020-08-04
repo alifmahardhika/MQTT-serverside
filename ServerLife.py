13e4e63887eafbb54c09df199656bf15e25ee456
alifmahardhika <alif.mahardhika@ui.ac.id>
2020-08-04 11:14:40 +0700

    add thingsboard publisher and conn ack listener

---
+++ ServerLife.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+'''
+========================================================================================================================
+NOTE: Program untuk menerima published mqtt messages. Adalah MQTT Client jadi basically bisa dijalanin dari mana aja 
+tidak harus di execute diserver. 
+NOTE: Execute di terminal manapun tidak harus di server.
+NOTE: Tiap database maksimal hanya 1 program ini yang jalan, kalau lebih nanti ada database entry yang duplicate
+NOTE: Requirements: pip install paho-mqtt
+========================================================================================================================
+'''
+import paho.mqtt.client as mqtt  # perlu pip install dulu
+from SensorDataToDB import sensor_data_handler
+
+
+# MQTT Settings
+MQTT_BROKER = "app.itsmyhealth.id"
+MQTT_PORT = 1882
+Keep_Alive_Interval = 45
+# kalau ada multiple topics bisa pakai wildcard
+MQTT_TOPIC = "/server-connection"
+
+
+'''
+================================================================
+Callback Functions, dipanggil sebagai hook dari client mqtt
+================================================================
+'''
+
+
+# Checks Result Code. RC = 0 = successful connection, other = refused
+def on_connect(mosq, obj, flags, rc):
+    if rc != 0:
+        pass
+        print("Unable to connect to MQTT Broker...")
+    else:
+        print("Connected with MQTT Broker: " + str(MQTT_BROKER))
+        # calls subscribe function, triggers on_subscribe callback
+        mqttc.subscribe(MQTT_TOPIC, 0)
+
+
+# ditrigger kalau ada message dengan topic yang di subscribe
+# akan memanggil function data handler dari file SensorDataToDB.py
+def on_message(mosq, obj, msg):
+    print("MQTT Data Received...")
+    print("MQTT Topic: " + msg.topic)
+    print("Data: " + msg.payload.decode('utf-8'))  # call function data handler
+    mqttc.publish("/server-response", '{"isConnected":true}')
+    print("PUBLISHED RESPONSE")
+
+
+# dipakai buat debug
+def on_subscribe(mosq, obj, mid, granted_qos):
+    # print('called subs')
+    pass
+
+
+'''
+================================================================
+Main process yang diexecute:
+1. configure mqtt client connection
+2. assign event callbacks
+3. connect to mqtt broker
+4. start a forever loop
+
+Program tidak akan diterminate kecuali ada interrupt dari system
+================================================================
+'''
+print('start listener')
+mqttc = mqtt.Client()
+mqttc.username_pw_set("sens1", "testing1")
+
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+print('Finished setup, connecting ...')
+
+# Connect
+mqttc.connect(MQTT_BROKER, int(MQTT_PORT), int(Keep_Alive_Interval))
+print('Connected and subscribed to topic ' + MQTT_TOPIC)
+
+# Continue the network loop
+mqttc.loop_forever()
---
+++ TBPublisher.py
@@ -0,0 +1,101 @@
+'''
+========================================================================================================================
+Program untuk simulasi mqtt message publishing. Dipakai untuk testing. Credentials database perlu disesuaikan.
+Tidak perlu diexecute
+Requirements: pip install paho-mqtt, pip install getmac 
+========================================================================================================================
+'''
+import paho.mqtt.client as mqtt
+import random
+import threading
+import json
+from getmac import get_mac_address as gma  # simulate mac address
+from datetime import datetime
+from math import floor
+
+# MQTT Settings
+MQTT_Broker = "app.itsmyhealth.id"
+MQTT_Port = 1883
+Keep_Alive_Interval = 45
+MQTT_TOPIC_TEMPERATURE = "v1/devices/me/telemetry"
+
+
+'''
+================================================================
+Callback Functions, dipanggil sebagai hook dari client mqtt
+================================================================
+'''
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc != 0:
+        pass
+        print("Unable to connect to MQTT Broker...")
+    else:
+        print("Connected with MQTT Broker: " + str(MQTT_Broker))
+
+
+def on_publish(client, userdata, mid):
+    pass
+
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        pass
+
+
+'''
+================================================================
+Program Functions
+================================================================
+'''
+
+
+# Publisher topic dan payload
+def publish_to_topic(topic, message):
+    mqttc.publish(topic, message)
+    print("Published: " + str(message) + " " + "on MQTT Topic: " + str(topic))
+
+
+# Dummy data untuk testing. Struktur payload di publisher asli HARUS sama dengan struktur payload disini
+def dummy_payload_builder():
+    threading.Timer(3.0, dummy_payload_builder).start()
+    temperature_dummy_value = float("{0:.2f}".format(random.uniform(33, 39)))
+
+    payload = {}
+    mac = gma()[0:-2]
+    # dummy mac addresses (5 different mac address)
+    mac += str(floor(random.uniform(10, 15)))
+    payload['sensor_mac_addr'] = mac
+    payload['time_stamp'] = (
+        datetime.today()).strftime("%d-%b-%Y %H:%M:%S:%f")
+    payload['temperature'] = temperature_dummy_value
+    payload['isAlert'] = False
+    payload_json_data = json.dumps(payload)
+
+    print("Publishing dummy Temperature Value: " +
+          str(temperature_dummy_value) + "...")
+    publish_to_topic(MQTT_TOPIC_TEMPERATURE, payload_json_data)
+
+
+'''
+================================================================
+Main process, diexecute saat program dijalankan lewat terminal
+================================================================
+'''
+# Set konfigurasi mqtt client dan credentials
+mqttc = mqtt.Client()
+mqttc.on_connect = on_connect
+mqttc.on_disconnect = on_disconnect
+mqttc.on_publish = on_publish
+mqttc.username_pw_set("Y1RmDk5xNj1C5KfyxRLG", None)
+
+# connect ke mqtt broker
+print("connecting")
+mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
+
+# build and publish dummy mqtt messages, akan diterima ListenerMQTT.py
+dummy_payload_builder()
+
+# PENTING: somehow kalau loop mqtt clientnya gak forever clientnya gamau connect
+mqttc.loop_forever()
